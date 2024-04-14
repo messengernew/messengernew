0d080e70c80e6b23218697e294efd6a08c9ef7cb
monkaCat.jar | MeSSengeR.py <139318962+messengernew@users.noreply.github.com>
2024-04-14 11:01:34 +0300

    Create git-install.sh

---
+++ git-install.sh
@@ -0,0 +1,4 @@
+curl -sSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl -sSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+curl -sSL https://git-scm.com/download/linux -o git-install.sh
+chmod +x git-install.sh
